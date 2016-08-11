39b9aa7bef6f2d13d542c31dc637e3c2a4c51bdf
Nick Bargnesi <nick@den-4.com>
2016-08-10 21:41:33 -0400

    tweak grnl main; basics

--- src/grnl/main.go
+++ src/grnl/main.go
@@ -23,113 +23,47 @@ See http://formwork-io.github.io/ for more.
 
 package main
 
-import "log"
 import "os"
 import "os/signal"
 import "fmt"
 import "syscall"
-import "time"
 import "goczmq-1.0"
+import cr "core"
 
 func main() {
 	info := "greenline: notoriously unreliable\n" +
 		"https://github.com/formwork-io/greenline\n" +
 		"This is free software with ABSOLUTELY NO WARRANTY."
 	fmt.Printf("%s\n--\n", info)
-	/*
-		var rails []Rail
-		var err error
-		rails, err = ReadEnvironment()
-		if err != nil {
-			die(err.Error())
-		}
-		pprint("configuring %d rails", len(rails))
-	*/
-
-	pprint("greenline alive")
+	cr.Out("greenline alive")
 	exitchan := make(chan os.Signal, 0)
 	signal.Notify(exitchan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
 		sig := <-exitchan
-		out("received %s signal, exiting.\n", sig.String())
+        if sig == nil {
+            return
+        }
+		cr.Out("got %s, exiting", sig.String())
 		os.Exit(0)
 	}()
 
 	reloadchan := make(chan int)
-	/*
-		go reloader(reloadchan)
-	*/
-	readychan := make(chan bool)
-	pollchan := make(chan bool)
-	go func() {
-		for {
-			readychan <- false
-			break
-		}
-	}()
-	pprint("greenline ready")
-	router, err := goczmq.NewRouter("tcp://*:5555")
-	if err != nil {
-		log.Fatal(err)
-	}
+	go cr.Reloader(reloadchan)
+	cr.Out("greenline ready")
+	router := cr.FirstOrDie(goczmq.NewRouter("tcp://*:5555")).(*goczmq.Sock)
 	defer router.Destroy()
 
-	log.Println("router created and bound")
+	cr.Out("router created and bound")
 	for {
 		select {
-		case reloadOp := <-reloadchan:
-			/*
-				if reloadOp&BinReload == BinReload {
-					pprint("new binary available, restarting greenline")
-					// exec or die
-					restart()
-				} else if reloadOp&ConfigReload == ConfigReload {
-					pprint("new configuration available, restarting greenline")
-					// exec or die
-					restart()
-				}
-			*/
-			fmt.Printf("%s\n", reloadOp)
-		case ready := <-readychan:
-			if !ready {
-				die("ready set fail")
-			}
-			// ready set go
-			pollchan <- true
+		case _ = <-reloadchan:
+			cr.Out("new binary available, restarting greenline")
+			cr.Out("RESART NOW")
+	        signal.Stop(exitchan)
+			router.Destroy()
+			close(exitchan)
+			close(reloadchan)
+			cr.Restart()
 		}
 	}
 }
-
-func makeMsg(msg string, args ...interface{}) string {
-	const layout = "%d%02d%02d-%02d-%02d-%02d greenline[%d]: %s"
-	now := time.Now()
-	year := now.Year()
-	month := now.Month()
-	day := now.Day()
-	hour := now.Hour()
-	minute := now.Minute()
-	seconds := now.Second()
-	pid := os.Getpid()
-	arg := fmt.Sprintf(msg, args...)
-	ret := fmt.Sprintf(layout, year, month, day, hour, minute, seconds, pid, arg)
-	return ret
-}
-
-func pprint(msg string, args ...interface{}) {
-	msg = makeMsg(msg, args...)
-	fmt.Fprintf(os.Stdout, msg+"\n")
-}
-
-func out(msg string, args ...interface{}) {
-	msg = makeMsg(msg, args...)
-	fmt.Fprintf(os.Stdout, msg)
-	os.Stdout.Sync()
-}
-
-func die(msg string, args ...interface{}) {
-	msg = makeMsg(msg, args...)
-	fmt.Fprintf(os.Stderr, msg+"\n")
-	os.Exit(1)
-}
-
-// vim: ts=4 noexpandtab
